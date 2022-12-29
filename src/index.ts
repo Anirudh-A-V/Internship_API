6bccf68318ce07a522e7370410594339bc12cdf2
Anirudh A V <anirudh.av02@gmail.com>
2022-12-29 13:29:08 +0530

    feat: cloudinary connect & express-session config

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,31 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+app.use(cookieParser());
+app.use(session({
+    secret: `${process.env.SECRET}`,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7 
+    }
+}));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
