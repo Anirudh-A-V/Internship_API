98b1a6f90b0b3259588d497e01c9e12fdcc7bd2e
Anirudh A V <anirudh.av02@gmail.com>
2022-12-29 14:03:26 +0530

    fix: MongoDB connection fixed

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,11 @@ app.listen(port, () => {
 });
 
 mongoose.set('strictQuery', true);
-mongoose.connect(`${process.env.MONGO_URI}`, () => {
+mongoose.connect(`${process.env.MY_CONNECTION_URL}`
+).then(() => {
     console.log("Connected to MongoDB");
+}
+).catch((err) => {
+    console.log("Error connecting to MongoDB", err);
 });
+
