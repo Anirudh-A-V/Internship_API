4aed5a188586b4daac121b9031a4df4bd49f7c1b
Anirudh A V <anirudh.av02@gmail.com>
2022-12-31 16:17:05 +0530

    feat: job routes added

---
+++ src/routes/job-routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import { appliedJobsHandler, applyJobHandler, createJobHandler, fetchApplicantDetailsHandler, hireApplicantHandler, providedJobsHandler, recentJobsHandler, selectApplicantHandler } from '../handlers/job-handler';
+
+const router = express.Router();
+
+router.post('/create-job', createJobHandler);
+router.post('/apply-job', applyJobHandler);
+router.post('/select-applicant', selectApplicantHandler);
+router.post('/hire-applicant', hireApplicantHandler);
+router.post('/fetch-applicants', fetchApplicantDetailsHandler);
+router.get('/jobs-provided', providedJobsHandler);
+router.get('/jobs-applied', appliedJobsHandler);
+router.get('/recent-jobs', recentJobsHandler);
+
+export default router;
