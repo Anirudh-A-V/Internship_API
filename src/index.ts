f2cbaa73752aba3e894092070191bf452f34b7a5
Anirudh A V <anirudh.av02@gmail.com>
2022-12-31 16:48:59 +0530

    refactor: cookie params changed

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,26 @@ import { v2 as cloudinary } from "cloudinary";
 import session from "express-session";
 import cookieParser from "cookie-parser";
 import passport from "passport";
-import passportLocal from "passport-local";
 
 import User from "./models/user";
 
 const app = express();
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
+
+app.use(session({
+    secret: `${process.env.SECRET}`,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        sameSite: 'none',
+        secure: true,
+        httpOnly: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}));
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 dotenv.config();
 
 cloudinary.config({
@@ -23,15 +36,14 @@ cloudinary.config({
 });
 
 app.use(cookieParser());
-app.use(session({
-    secret: `${process.env.SECRET}`,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        secure: false,
-        maxAge: 1000 * 60 * 60 * 24 * 7 
-    }
-}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.header('origin'));
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -42,7 +54,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.send("Welcome to Internship-cell CET API");
 });
 
 const port = process.env.PORT || 3000;
