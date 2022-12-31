47c8e5069a6fe497fe559d737d5a805e976db6dd
Anirudh A V <anirudh.av02@gmail.com>
2022-12-31 16:30:17 +0530

    refactor: upload object taken from seeker-routes

--- src/routes/seeker-routes.ts
+++ src/routes/seeker-routes.ts
@@ -1,17 +1,11 @@
 import express from 'express';
-import multer from 'multer';
 
 import { seekerRegistrationHandler, seekerEditHandler, resumeUploadHandler, seekerSearchHandler } from '../handlers/seeker-handler';
+import { upload } from '../utils/Upload';
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    filename: (req, file, cb) => {
-        cb(null, `${file.fieldname}-${Date.now()}`);
-    },
-});
 
-const upload = multer({ storage });
 
 router.post('/register', seekerRegistrationHandler);
 router.post('/upload-resume', upload.single('resume'), resumeUploadHandler);
