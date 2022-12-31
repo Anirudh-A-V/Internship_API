83348c487e68be088e8296d0d1af1021a2aa2580
Anirudh A V <anirudh.av02@gmail.com>
2022-12-31 16:23:27 +0530

    feat: provider-routes defined

---
+++ src/routes/provider-routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import { providerRegistrationHandler, providerEditHandler, fetchProvidersHandler, getSeekerResume } from '../handlers/provider-handler';
+
+const router = express.Router();
+
+router.post('/register', providerRegistrationHandler);
+router.post('/edit', providerEditHandler);
+router.get('/fetch-providers', fetchProvidersHandler);
+router.post('/resume-download', getSeekerResume);
+
+export default router;
