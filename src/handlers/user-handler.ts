6194b63eb12be8610295ce0817c318b0437494a4
Anirudh A V <anirudh.av02@gmail.com>
2022-12-31 16:10:50 +0530

    feat: user-handler added

---
+++ src/handlers/user-handler.ts
@@ -0,0 +1,102 @@
+import { NextFunction, Request, Response } from 'express';
+import { v2 } from 'cloudinary';
+import passport from 'passport';
+
+import User from '../models/user';
+import { createUser, getFullUserById } from '../services/user-services';
+
+const signinHandler = async (req: Request, res: Response, next: NextFunction) => {
+    await passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+
+        return req.logIn(user, async (error) => {
+            if (error) {
+                return next(info);
+            }
+
+            const userDetails = await getFullUserById(user);
+            // Send a redirect message with a status code 307
+            if (!userDetails) return res.redirect(307, '/login');
+
+            // Add a unique status code inside the response data
+            return res.status(200).json({
+                status: 'success',
+                data: userDetails
+            });
+        });
+    })(req, res, next);
+};
+
+const signupHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { userType, email, firstName, lastName } = req.body;
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(409).json({ message: 'User already exists!' });
+        }
+
+        await createUser(req.body);
+
+        return passport.authenticate('local', (err, user) => {
+            if (err) {
+                return next(err);
+            }
+
+            return req.logIn(user, (error) => {
+                if (error) {
+                    return next(error);
+                }
+                return res.status(200).json({ id: user._id, userType, firstName, lastName });
+            });
+        })(req, res, next);
+    } catch (err) {
+        console.log('file name: user-handler.ts')
+        console.log(err);
+        next(err);
+    }
+};
+
+const isAuthenicatedHandler = async (req: any, res: Response, next: NextFunction) => {
+    try {
+        const user = {
+            _id: req.user._id,
+            userType: req.user.userType,
+            firstName: req.user.firstName,
+            lastName: req.user.lastName,
+            image: req.user.image,
+        };
+        const userDetails = await getFullUserById(user);
+        // if (!userDetails) return res.status(401).json({ message: 'Unauthorized' });
+        return res.json(userDetails);
+    } catch (err) {
+        console.log('file name: user-handler.ts')
+        console.log(err);
+        next(err);
+    }
+};
+
+const imageUploadHandler = async (req: any, res: Response, next: NextFunction) => {
+    const data = { image: req.file.path };
+    try {
+        const uploadedImage = await v2.uploader.upload(data.image);
+        await User.findOneAndUpdate(
+            { _id: req.user._id },
+            { image: uploadedImage.url },
+          );
+          return res.json({ image: uploadedImage.url });
+    } catch (err) {
+        console.log('file name: user-handler.ts')
+        console.log(err);
+        next(err);
+    }
+};
+
+export {
+    signinHandler,
+    signupHandler,
+    isAuthenicatedHandler,
+    imageUploadHandler,
+};
